842760c91df2d354479b89911b0d971f407f4310
GrizzlyOne95 <143971298+GrizzlyOne95@users.noreply.github.com>
2026-09-11 15:51:59 -0500

    Test exact MakeTRN HGT smoothing pass

--- tests/test_maketrn_compat.py
+++ tests/test_maketrn_compat.py
@@ -27,19 +27,45 @@ class MakeTRNCompatTests(unittest.TestCase):
     def test_stock_trn_height_matches_blank_create(self):
         self.assertAlmostEqual(maketrn_compat.stock_trn_height(1234), 123.4)
 
-    def test_hgt_triangle_upsample_half_sample_cases(self):
+    def test_hgt_triangle_upsample_includes_make_trn_smoothing(self):
         source = np.array([[0, 10], [20, 40]], dtype=np.uint16)
         up = maketrn_compat.upsample_hgt_to_hg2(source)
         expected = np.array(
             [
-                [0, 5, 10, 10],
-                [10, 20, 25, 25],
-                [20, 30, 40, 40],
-                [20, 30, 40, 40],
+                [9, 12, 16, 18],
+                [14, 18, 23, 25],
+                [22, 26, 32, 35],
+                [25, 30, 37, 40],
             ],
             dtype=np.uint16,
         )
         np.testing.assert_array_equal(up, expected)
+
+    def test_make_trn_smoothing_uses_valid_neighbors_and_half_up_rounding(self):
+        source = np.array(
+            [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+            ],
+            dtype=np.uint16,
+        )
+        smoothed = maketrn_compat.smooth_make_trn_hg2(source)
+        expected = np.array(
+            [
+                [2, 3, 3],
+                [4, 4, 5],
+                [5, 6, 6],
+            ],
+            dtype=np.uint16,
+        )
+        np.testing.assert_array_equal(smoothed, expected)
+
+    def test_hgt_constant_surface_stays_constant_after_conversion(self):
+        source = np.full((4, 4), 1234, dtype=np.uint16)
+        up = maketrn_compat.upsample_hgt_to_hg2(source)
+        self.assertEqual(up.shape, (8, 8))
+        self.assertTrue(np.all(up == 1234))
 
     def test_hgt_zone_unpack_masks_12_bits_and_preserves_zone_order(self):
         zone_samples = maketrn_compat.HGT_SAMPLES_PER_ZONE ** 2
